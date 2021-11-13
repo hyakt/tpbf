9423dde6ca3b1f86fe9f1e55d97a5b2b89f1f1b0
hyakt <hyakt0@gmail.com>
2021-11-13 15:05:20 +0900

    setup lint and lint-staged

---
+++ .lintstagedrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  'src/**/*.ts?(x)': (filenames) =>
+    `next lint --fix --file ${filenames
+      .map((file) => file.split(process.cwd())[1])
+      .join(' --file ')}`,
+  'src/**/*.{ts?(x),css,md}': 'prettier --write',
+}
