272ce1e8b5dd1f8303207b8a9ba43360ae9f7abb
hyakt <hyakt0@gmail.com>
2021-11-15 19:04:40 +0900

    update ui/checkbox component to controllable

--- src/components/ui/checkbox/index.tsx
+++ src/components/ui/checkbox/index.tsx
@@ -1,12 +1,21 @@
 type Props = {
   id: string
+  checked: boolean
+  onChange: () => void
 }
 
 export const Checkbox: React.FC<Props> = (props) => {
-  const { id, children } = props
+  const { id, children, checked, onChange } = props
+
   return (
     <label htmlFor={id}>
-      <input type="checkbox" id={id} />
+      <input
+        data-testid="ui-checkbox-input"
+        type="checkbox"
+        id={id}
+        checked={checked}
+        onChange={onChange}
+      />
       {children}
     </label>
   )
