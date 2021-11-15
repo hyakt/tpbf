362f5040342cac72d46e0c5de052bb67c5240db7
hyakt <hyakt0@gmail.com>
2021-11-15 21:57:58 +0900

    update type of checkbox

--- src/components/ui/checkbox/index.tsx
+++ src/components/ui/checkbox/index.tsx
@@ -1,20 +1,19 @@
-type Props = {
-  id: string
-  checked: boolean
-  onChange: () => void
-}
+import styles from './styles/index.module.scss'
 
-export const Checkbox: React.FC<Props> = (props) => {
+type CheckboxProps = Omit<React.ComponentPropsWithoutRef<'input'>, 'type'>
+
+export const Checkbox: React.FC<CheckboxProps> = (props) => {
   const { id, children, checked, onChange } = props
 
   return (
-    <label htmlFor={id}>
+    <label className={styles.label} htmlFor={id}>
       <input
         data-testid="ui-checkbox-input"
         type="checkbox"
         id={id}
         checked={checked}
         onChange={onChange}
+        className={styles.checkbox}
       />
       {children}
     </label>
