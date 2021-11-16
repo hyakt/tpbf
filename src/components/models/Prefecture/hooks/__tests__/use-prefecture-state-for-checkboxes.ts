01ca23cc1d259ba9facb1edaf94e543f573ed313
hyakt <hyakt0@gmail.com>
2021-11-16 09:48:45 +0900

    add use-prefecture-state-for-checkboxes test case

--- src/components/models/Prefecture/hooks/__tests__/use-prefecture-state-for-checkboxes.ts
+++ src/components/models/Prefecture/hooks/__tests__/use-prefecture-state-for-checkboxes.ts
@@ -2,15 +2,27 @@ import { renderHook } from '@testing-library/react-hooks'
 import { usePrefectureStateForCheckboxes } from '../use-prefecture-state-for-checkboxes'
 
 describe('usePrefectureStateForCheckboxes', () => {
-  it('return values', () => {
-    const prefectures = ['北海道', '青森県']
-    const { result } = renderHook(() =>
-      usePrefectureStateForCheckboxes(prefectures)
-    )
+  describe('pass empty array', () => {
+    it('return values', () => {
+      const prefectures: string[] = []
+      const { result } = renderHook(() =>
+        usePrefectureStateForCheckboxes(prefectures)
+      )
 
-    expect(result.current.prefecturesState).toStrictEqual({
-      北海道: false,
-      青森県: false,
+      expect(result.current.prefecturesState).toStrictEqual({})
+    })
+  })
+  describe('pass fullfiled array', () => {
+    it('return values', () => {
+      const prefectures = ['北海道', '青森県']
+      const { result } = renderHook(() =>
+        usePrefectureStateForCheckboxes(prefectures)
+      )
+
+      expect(result.current.prefecturesState).toStrictEqual({
+        北海道: false,
+        青森県: false,
+      })
     })
   })
 })
